2491c50ce2932bf814947d25995d35b033d569e3
liuliuliu <liuliuliu@liu.com>
2019-01-16 13:38:53 +0800

    添加国际化功能

--- src/main/java/com/conf/ApplicationContext.java
+++ src/main/java/com/conf/ApplicationContext.java
@@ -2,7 +2,9 @@ package com.conf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.LocaleResolver;
 
 /**
  * @author liutianyang
@@ -15,5 +17,10 @@ public class ApplicationContext
 {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationContext.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
     }
 }
---
+++ src/main/java/com/conf/MyLocaleResolver.java
@@ -0,0 +1,30 @@
+package com.conf;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+/**
+ * @author liutianyang
+ * @create 2019-01-2019/1/16
+ */
+public class MyLocaleResolver implements LocaleResolver{
+    @Override
+    public java.util.Locale resolveLocale(HttpServletRequest request) {
+        String l = request.getParameter("l");
+        Locale locale = Locale.getDefault();
+        if(!StringUtils.isEmpty(l)){
+            String[] split = l.split("_");
+            locale = new Locale(split[0],split[1]);
+        }
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest request, HttpServletResponse response, java.util.Locale locale) {
+
+    }
+}
